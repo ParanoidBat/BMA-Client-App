778b3034f617138ea37a7b77ae1155a09fa3e241
omar azaz <cool_omer@hotmail.co.uk>
2022-07-12 13:20:48 +0500

    check api call

--- src/Contact.js
+++ src/Contact.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Contact() {
+  const [users, setUsers] = useState();
+
+  axios.get("https://bma-api-v1.herokuapp.com/user?page=1").then((res) => {
+    setUsers(res.data.data);
+  });
+
   return (
     <div>
       <h3>Contact</h3>
       <p>fgiugebgrebgrieugneljvnierfnj</p>
-      <p>
-        <a href="google.com">Google</a>
-      </p>
+      <div>
+        {users?.map((user, index) => (
+          <p key={index}> {user.name} </p>
+        ))}
+      </div>
     </div>
   );
 }
