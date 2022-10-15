535cc8d105605286a4368a2158408f02469fb12d
omar azaz <cool_omer@hotmail.co.uk>
2022-10-15 14:43:18 +0500

    back button on signup

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 import OrgSignup from "components/OrgSignup";
 import UserSignup from "components/UserSignup";
+import { ChevronLeft } from "@mui/icons-material";
+import { Button, Grid } from "@mui/material";
 
 export default function Signup({ setFirstPage }) {
   const [organizationID, setOrganizationID] = useState(null);
 
-  return !organizationID ? (
-    <OrgSignup setOrganizationID={setOrganizationID} />
-  ) : (
-    <UserSignup organizationID={organizationID} setFirstPage={setFirstPage} />
+  return (
+    <>
+      <Grid>
+        <Button onClick={() => setFirstPage("Login")}>
+          <ChevronLeft /> Login
+        </Button>
+      </Grid>
+      {!organizationID ? (
+        <OrgSignup setOrganizationID={setOrganizationID} />
+      ) : (
+        <UserSignup
+          organizationID={organizationID}
+          setFirstPage={setFirstPage}
+        />
+      )}
+    </>
   );
 }
